6039536129b68bf701a0642031960cb4d8615fc7
jmraggi <juanmanuelraggi@gmail.com>
2025-03-20 09:16:44 -0300

    Project

---
+++ app/Models/Comment.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Comment extends Model
+{
+    use HasFactory;
+
+    //ESTO NO SE HACE EN PRODUCCION SOLO PARA PRUEBAS.
+    protected $guarded = ['*'];
+
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+    
+}
---
+++ database/migrations/2025_03_19_104637_create_comments_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            // foreign busca tabla user_id y hace referencia a la columna id de la tabla users. Ondelete cascade borra los comentarios si se borra el usuario
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->string('text');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comments');
+    }
+};
